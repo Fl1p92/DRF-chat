dbf0c19956b574be1302b7361f5c97f8221617c1
Eugene <Fl1p92@ukr.net>
2019-01-05 17:52:05 +0200

    CHAT-5. Added django-debug-toolbar to project.

--- chat/urls.py
+++ chat/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
@@ -21,3 +22,7 @@ urlpatterns = [
     path('', include('apps.contact.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
