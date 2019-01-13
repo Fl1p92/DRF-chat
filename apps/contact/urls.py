2ca1a9a46103c909802111eb2cf9044ed39ee288
Eugene <Fl1p92@ukr.net>
2019-01-13 13:56:28 +0200

    CHAT-7. Updated logic for list contacts. Added api for delete user from contacts. Created object permissions for contactline.

--- apps/contact/urls.py
+++ apps/contact/urls.py
@@ -7,7 +7,8 @@ from . import views
 # API endpoints
 urlpatterns = format_suffix_patterns([
     path('', views.api_root),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('users/<int:pk>/contacts/', views.UserContacts.as_view(), name='contactslist-detail'),
+    path('users/', views.UserListAPIView.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetailAPIView.as_view(), name='user-detail'),
+    path('contacts/', views.ListCreateUserContactsAPIView.as_view(), name='contact-list-detail'),
+    path('contacts/<int:pk>/', views.DeleteUserContactAPIView.as_view(), name='contact-list-delete'),
 ])
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,36 +1,44 @@
 from django.contrib.auth.models import User
 
-from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from .models import ContactLine
-from .permissions import IsContactsOwner
+from .permissions import IsContactOwner
 from .serializers import UserSerializer, ContactLineSerializer
 
 
 @api_view(['GET'])
+@permission_classes((permissions.AllowAny, ))
 def api_root(request, format=None):
     return Response({
         'users': reverse('user-list', request=request, format=format),
+        'contacts': reverse('contact-list-detail', request=request, format=format),
     })
 
 
-class UserList(generics.ListAPIView):
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny, )
+
+
+class UserDetailAPIView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserContacts(generics.ListCreateAPIView):
+class ListCreateUserContactsAPIView(generics.ListCreateAPIView):
     serializer_class = ContactLineSerializer
-    permission_classes = (IsContactsOwner, )
 
     def get_queryset(self):
-        queryset = ContactLine.objects.filter(contacts_list__owner__pk=self.kwargs['pk']).prefetch_related('contact')
+        queryset = ContactLine.objects.filter(contacts_list__owner=self.request.user).prefetch_related('contact')
         return queryset
+
+
+class DeleteUserContactAPIView(generics.DestroyAPIView):
+
+    def get_queryset(self):
+        return ContactLine.objects.filter(contacts_list__owner=self.request.user)
