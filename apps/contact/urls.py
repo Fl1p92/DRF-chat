bc4abd4dd51e3514033a02d29f3f439001840a3e
Eugene <Fl1p92@ukr.net>
2019-01-18 14:29:22 +0200

    CHAT-19. Added API to check for user existence.

--- apps/contact/urls.py
+++ apps/contact/urls.py
@@ -10,6 +10,7 @@ urlpatterns = format_suffix_patterns([
     path('users/', views.UserListAPIView.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetailAPIView.as_view(), name='user-detail'),
     path('users/<int:pk>/change-password/', views.UserPasswordChangeAPIView.as_view(), name='user-password-change'),
+    path('users/check-user-exists/', views.UserCheckExistsAPIView.as_view(), name='user-check-exists'),
     path('contacts/', views.ListCreateUserContactsAPIView.as_view(), name='contact-list-detail'),
     path('contacts/<int:pk>/', views.DeleteUserContactAPIView.as_view(), name='contact-list-delete'),
 ])
